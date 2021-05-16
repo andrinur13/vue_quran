de5676520312c63cc73fbf16a60b0e5c461b4191
andrinur13 <andribis13@gmail.com>
2021-05-16 10:55:03 +0700

    init app and vuesax

---
+++ src/components/Alert.vue
@@ -0,0 +1,19 @@
+<template>
+  <vs-alert
+    title="Lorem ipsum dolor sit amet"
+    color="rgb(231, 154, 23)"
+    active="true"
+  >
+    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+    quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+    consequat
+  </vs-alert>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
---
+++ src/views/Quran.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+      <alert />
+  </div>
+</template>
+
+<script>
+import Alert from '../components/Alert.vue';
+
+export default {
+  components: { Alert },
+
+}
+</script>
+
+<style>
+
+</style>
